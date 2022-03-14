c731ada2a2d95a6f39dc21427b5b5cff5efb722d
Thiago Rod <whytigo@gmail.com>
2022-03-14 00:12:29 -0400

    added the rest of the API routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,16 +1,61 @@
 const router = require('express').Router();
-//Require the correct files from the models and authorizations
+
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth.js');
 
 router.post('/', withAuth, async (req, res) => {
-//Complete the asychronous function with error handling
+  try {
+    const postData = await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      user_id: req.session.user_id
+    });
+    //console.log(postData);
+    res.status(200).json(postData);
+  } catch (error) {
+    res.status(400).json("Could not make post.")
+  }
 });
 
 router.put('/:id', withAuth, async (req, res) => {
-//Complete the asychronous function with error handling
+  try {
+    const postData = Post.update({
+      title: req.body.title,
+      body: req.body.body,
+    },{
+      where:{
+        id: req.params.id
+      }
+    });
+    
+    if(!postData){
+      res.status(400).json("Could not find post!");
+      return;
+    }
+
+    res.status(200).json("postData");
+  } catch (error) {
+    res.status(400).json("Could not make post.")
+  }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
-  //Complete the asychronous function with error handling
+  try {
+    const postData = Post.destroy({
+      where:{
+        id: req.params.id
+      }
+    });
+
+    if(!postData){
+      res.status(400).json("Could not find post!");
+      return;
+    }
+
+    res.status(200).json("postData");
+  } catch (error) {
+    res.status(400).json("Could not make post.")
+  }
 });
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,19 +1,58 @@
 const router = require('express').Router();
-//Require the correct files from the models
+const { Post, Comment, User } = require('../../models');
 
 router.post('/', async (req, res) => {
-  // Create the correct asychronous function for this login post request with error handling
-
+  try {
+    const userData = await User.create(req.body);
+    // Save the user Id and the Login information 
+    req.session.save(function() {
+      req.session.user_id = userData.id;
+      req.session.loggedIn = true;
+      console.log(req.session.loggedIn);
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/login', async (req, res) => {
-  // Create the correct asychronous function for this login post request with error handling
+  try{
+    const userData = await User.findOne({where: {username: req.body.username}});
 
+    if(!userData){
+      res.status(400).json({message: 'Invalid Username!'});
+      console.log('Invalid Username!');
+      return;
+    }
+
+    const validPassword = await userData.verifyPassword(req.body.password);
+
+    if(!validPassword){
+      res.status(400).json({message: 'Invalid Password!'});
+      console.log('Invalid Password!');
+      return;
+    }
+    // Save the user Id and the Login information 
+    req.session.save(function() {
+      req.session.user_id = userData.id;
+      req.session.loggedIn = true;
+      console.log(req.session.loggedIn);
+      res.status(200).json(userData);
+    });
+  }catch(err){
+    res.status(400).json(err);
+  }
 });
 
 router.post('/logout', (req, res) => {
-  // Create the correct functionality for this post request with error handling
-
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
