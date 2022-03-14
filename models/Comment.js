ea4b868c9c0adab0e5a5d140727d6923fffdbec5
Thiago Rod <whytigo@gmail.com>
2022-03-13 23:20:41 -0400

    made all the Models for the DB using sequilize structure

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,39 @@
 class Comment extends Model {}
 
 Comment.init(
-  // Fill in the missing data
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+      onDelete: "CASCADE"
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
 );
 
 module.exports = Comment;
